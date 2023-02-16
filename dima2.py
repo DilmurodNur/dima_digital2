46d011319b2d6dfa5921e9b6becd6a487e0436ca
student <nuraddinovdilmurod@gmail.com>
2023-02-16 15:28:52 +0300

    vtoraya rab

---
+++ dima2.py
@@ -0,0 +1,9 @@
+'2 variant'
+pas = input("vvedite parol:")
+if len(pas) < 8:
+    print("slishkom korotkiy")
+elif pas [0:7] == "sigmaboy" :
+    print('nenajdyonniy parol')
+
+else:
+    print("OK")
--- dima_digital_zadacaha1.py
+++ dima_digital_zadacaha1.py
@@ -5,3 +5,4 @@ if a == b:
     print('parol true')
 else:
     print('parol false')
+
